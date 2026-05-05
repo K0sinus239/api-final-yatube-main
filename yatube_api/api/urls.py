9e30261db7f0f51872a0af2d4ba112d474c9b7c3
Константин Белов <belov.konstantin.a@gmail.com>
2026-05-05 09:34:03 +0300

    16

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,11 +1,38 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 
-from api.views import PostViewSet, CommentViewSet
+from api.views import (
+    PostViewSet,
+    GroupViewSet,
+    FollowViewSet,
+    CommentViewSet,
+)
 
 v1_router = DefaultRouter()
 v1_router.register("posts", PostViewSet, basename="posts")
+v1_router.register("groups", GroupViewSet, basename="groups")
+v1_router.register("follow", FollowViewSet, basename="follow")
 v1_router.register("comments", CommentViewSet, basename="comments")
+
 urlpatterns = [
     path("", include(v1_router.urls)),
+    re_path(
+        r"^posts/(?P<post_id>\d+)/comments/$",
+        CommentViewSet.as_view(
+            {"get": "list", "post": "create"}
+        ),
+        name="post-comments-list",
+    ),
+    re_path(
+        r"^posts/(?P<post_id>\d+)/comments/(?P<pk>\d+)/$",
+        CommentViewSet.as_view(
+            {
+                "get": "retrieve",
+                "put": "update",
+                "patch": "partial_update",
+                "delete": "destroy",
+            }
+        ),
+        name="post-comments-detail",
+    ),
 ]
--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 from django.views.generic import TemplateView
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls')),
-    path('api/v1/', include('djoser.urls')),
-    path('api/v1/', include('djoser.urls.jwt')),
+    path("admin/", admin.site.urls),
+    path("api/v1/", include("api.urls")),
+    path("api/v1/", include("djoser.urls")),
+    path("api/v1/", include("djoser.urls.jwt")),
     path(
-        'redoc/',
-        TemplateView.as_view(template_name='redoc.html'),
-        name='redoc'
+        "redoc/",
+        TemplateView.as_view(template_name="redoc.html"),
+        name="redoc"
     ),
 ]
