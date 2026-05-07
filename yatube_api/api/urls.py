ac99d7f87206eb49e0da8fdab8a4c854c11ca3ce
Константин Белов <belov.konstantin.a@gmail.com>
2026-05-07 18:02:53 +0300

    27

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -12,7 +12,7 @@ v1_router = DefaultRouter()
 v1_router.register("posts", PostViewSet, basename="posts")
 v1_router.register("groups", GroupViewSet, basename="groups")
 v1_router.register("follow", FollowViewSet, basename="follow")
-v1_router.register(r"posts/(?P<post_pk>[^/.]+)/comments", CommentViewSet,
+v1_router.register(r"posts/(?P<post_id>[^/.]+)/comments", CommentViewSet,
                    basename="comments")
 
 urlpatterns = [
